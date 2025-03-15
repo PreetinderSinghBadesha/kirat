6b2b4268ca1e6fe06de5085889b242e9afc5f7e0
preetindersinghbadesha <preetindersingh13per@gmail.com>
2025-03-15 23:27:44 +0530

    Update Next.js configuration for static export and add build script

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "i.pinimg.com",
-        },
-      ],
-    },
-  };
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
