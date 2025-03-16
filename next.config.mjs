2b602c5720ea3916701ee924a9172bdba25e785f
preetindersinghbadesha <preetindersingh13per@gmail.com>
2025-03-16 09:16:43 +0530

    Enhance Next.js configuration for development and add build/deploy scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "i.pinimg.com",
-        },
-      ],
-    },
-  };
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
 export default nextConfig;
